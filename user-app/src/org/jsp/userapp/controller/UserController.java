c68ade8a55a325250452aeb8cdb37b5d894f13e3
sathishnyadav <90254530+sathishnyadav@users.noreply.github.com>
2023-11-18 13:59:32 +0530

    Implementation of CRUD using JDBC

--- user-app/src/org/jsp/userapp/controller/UserController.java
+++ user-app/src/org/jsp/userapp/controller/UserController.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 import org.jsp.userapp.dao.UserDao;
+import org.jsp.userapp.dto.User;
 
 public class UserController {
 	public static void main(String[] args) {
@@ -15,8 +16,8 @@ public class UserController {
 		while (flag) {
 			System.out.println("1.Save User");
 			System.out.println("2.Update User");
-			System.out.println("3.Verify User by email and password");
-			System.out.println("4.Verify User by phone and password");
+			System.out.println("3.Verify User by phone and password");
+			System.out.println("4.Verify User by email and password");
 			System.out.println("5.Find user By Id");
 			System.out.println("6.delete User By Id");
 			System.out.println("7.Exit");
@@ -28,8 +29,80 @@ public class UserController {
 				long phone = s.nextLong();
 				String email = s.next();
 				String password = s.next();
-				String message = dao.saveUser(id, name, phone, email, password);
+				User u = new User(id, name, phone, email, password);
+				String message = dao.saveUser(u);
 				System.out.println(message);
+				break;
+			}
+			case 2: {
+				System.out.println("Enter the use id,name,phone,email and password to update user");
+				int id = s.nextInt();
+				String name = s.next();
+				long phone = s.nextLong();
+				String email = s.next();
+				String password = s.next();
+				User u = new User(id, name, phone, email, password);
+				String message = dao.updateUser(u);
+				System.out.println(message);
+				break;
+			}
+
+			case 3: {
+				System.out.println("Enter the phone Number and password to verify user");
+				long phone = s.nextLong();
+				String password = s.next();
+				User u = dao.verifyUser(phone, password);
+				if (u != null) {
+					System.out.println("User Verified Succesfully");
+					System.out.println("User Id:" + u.getId());
+					System.out.println("User Name:" + u.getName());
+					System.out.println("Email Id:" + u.getEmail());
+					System.out.println("Phone Number:" + u.getPhone());
+				} else {
+					System.err.println("Invalid Phone Number or Password");
+				}
+				break;
+			}
+			case 4: {
+				System.out.println("Enter the Email Id and password to verify user");
+				String email = s.next();
+				String password = s.next();
+				User u = dao.verifyUser(email, password);
+				if (u != null) {
+					System.out.println("User Verified Succesfully");
+					System.out.println("User Id:" + u.getId());
+					System.out.println("User Name:" + u.getName());
+					System.out.println("Email Id:" + u.getEmail());
+					System.out.println("Phone Number:" + u.getPhone());
+				} else {
+					System.err.println("Invalid Email Id or Password");
+				}
+				break;
+			}
+			case 5: {
+				System.out.println("Enter the User id to find the user");
+				int id = s.nextInt();
+				User u = dao.findUserById(id);
+				if (u != null) {
+					System.out.println("User Found");
+					System.out.println("User Id:" + u.getId());
+					System.out.println("User Name:" + u.getName());
+					System.out.println("Email Id:" + u.getEmail());
+					System.out.println("Phone Number:" + u.getPhone());
+				} else {
+					System.err.println("Invalid Id");
+				}
+				break;
+			}
+			case 6: {
+				System.out.println("Enter the User id to delete the record");
+				int id = s.nextInt();
+				boolean deleted = dao.deleteUser(id);
+				if (deleted) {
+					System.out.println("User deleted");
+				} else {
+					System.err.println("cannot delete user");
+				}
 				break;
 			}
 			case 7: {
--- user-app/src/org/jsp/userapp/dao/UserDao.java
+++ user-app/src/org/jsp/userapp/dao/UserDao.java
@@ -9,6 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.jsp.userapp.dto.User;
+
 public class UserDao {
 	Connection con;
 	PreparedStatement pst;
@@ -33,15 +35,15 @@ public class UserDao {
 		}
 	}
 
-	public String saveUser(int id, String name, long phone, String email, String password) {
+	public String saveUser(User u) {
 		String qry = "insert into user values(?,?,?,?,?)";
 		try {
 			pst = con.prepareStatement(qry);
-			pst.setInt(1, id);
-			pst.setString(2, name);
-			pst.setLong(3, phone);
-			pst.setString(4, email);
-			pst.setString(5, password);
+			pst.setInt(1, u.getId());
+			pst.setString(2, u.getName());
+			pst.setLong(3, u.getPhone());
+			pst.setString(4, u.getEmail());
+			pst.setString(5, u.getPassword());
 			pst.executeUpdate();
 			return "user saved";
 		} catch (SQLException e) {
@@ -49,6 +51,107 @@ public class UserDao {
 		}
 
 	}
+
+	public String updateUser(User u) {
+		String qry = "update user set name=?,phone=?,email=?,password=? where id=?";
+		try {
+			pst = con.prepareStatement(qry);
+			pst.setInt(5, u.getId());
+			pst.setString(1, u.getName());
+			pst.setLong(2, u.getPhone());
+			pst.setString(3, u.getEmail());
+			pst.setString(4, u.getPassword());
+			pst.executeUpdate();
+			return "user updated";
+		} catch (SQLException e) {
+			return "Unable to update user";
+		}
+
+	}
+
+	public User verifyUser(long phone, String password) {
+		String qry = "select * from user where phone=? and password=?";
+		try {
+			pst = con.prepareStatement(qry);
+			pst.setLong(1, phone);
+			pst.setString(2, password);
+			rs = pst.executeQuery();
+			User u = new User();
+			if (rs.next()) {
+				u.setId(rs.getInt(1));
+				u.setName(rs.getString(2));
+				u.setPhone(rs.getLong(3));
+				u.setEmail(rs.getString(4));
+				u.setPassword(rs.getString(5));
+				return u;
+			}
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public User verifyUser(String email, String password) {
+		String qry = "select * from user where email=? and password=?";
+		try {
+			pst = con.prepareStatement(qry);
+			pst.setString(1, email);
+			pst.setString(2, password);
+			rs = pst.executeQuery();
+			User u = new User();
+			if (rs.next()) {
+				u.setId(rs.getInt(1));
+				u.setName(rs.getString(2));
+				u.setPhone(rs.getLong(3));
+				u.setEmail(rs.getString(4));
+				u.setPassword(rs.getString(5));
+				return u;
+			}
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public boolean deleteUser(int id) {
+		String sql = "delete from user where id=?";
+		try {
+			pst = con.prepareStatement(sql);
+			pst.setInt(1, id);
+			int r = pst.executeUpdate();
+			if (r == 1)
+				return true;
+			return false;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public User findUserById(int id) {
+		String sql = "select * from user where id=?";
+		try {
+			pst = con.prepareStatement(sql);
+			pst.setInt(1, id);
+			rs = pst.executeQuery();
+			User u = new User();
+			if (rs.next()) {
+				u.setId(rs.getInt(1));
+				u.setName(rs.getString(2));
+				u.setPhone(rs.getLong(3));
+				u.setEmail(rs.getString(4));
+				u.setPassword(rs.getString(5));
+				return u;
+			}
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	public String exit() throws SQLException {
 		if (con != null)
 			con.close();
